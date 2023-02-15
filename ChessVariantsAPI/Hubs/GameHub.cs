c81ce5667ad584d9077bea94b8224e957ecc6bbb
Johan Selin <johanselin99@gmail.com>
2023-02-15 17:07:11 +0100

    Added GameHub with MovePiece event
    Co-Authored-By: Joakim-Holmberg <89933865+Joakim-Holmberg@users.noreply.github.com>

---
+++ ChessVariantsAPI/Hubs/GameHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChessVariantsAPI.Hubs;
+
+public class GameHub : Hub
+{
+    public async Task MovePiece(string move)
+    {
+        // if move is valid, compute new board
+        await Clients.All.SendAsync("pieceMoved", "board"); // TODO only respond to players of the particular game
+        // else respond with illegal move
+        //await Clients.Caller.SendAsync("illegalMove", "board");
+    }
+}
