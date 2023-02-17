c6de8d71ba8dee55d0d3d6b0ee2b328355cb6d1d
ValleSten <valdemar.stenhammar@hotmail.com>
2023-02-17 16:28:38 +0100

    Add test used for manual debugging

--- ChessVariantsLogic.Tests/MyUnitTest.cs
+++ ChessVariantsLogic.Tests/MyUnitTest.cs
@@ -3,6 +3,7 @@ using Xunit;
 
 namespace ChessVariantsLogic.Tests;
 
+// These tests are obviously not done, they have been used for manual debugging so don't worry :))
 public class MyUnitTest
 {
 
@@ -24,6 +25,21 @@ public class MyUnitTest
         Console.WriteLine(board.ReadBoardAsFEN());
         board.MakeMove("h2h4");
         Console.WriteLine(board.ReadBoardAsFEN());
+
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void Test_sizes()
+    {
+        var board1 = new Chessboard(6,6);
+        Console.WriteLine(board1.ToString());
+
+        var board2 = new Chessboard(6,15);
+        Console.WriteLine(board2.ToString());
+        
+        var board3 = new Chessboard(10,1);
+        Console.WriteLine(board3.ToString());
 
         Assert.True(true);
     }
