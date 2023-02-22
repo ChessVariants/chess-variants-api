3e50d7e981b154862c9f95739c0ec0ec43855450
Johan Selin <johanselin99@gmail.com>
2023-02-22 15:36:13 +0100

    Draft for check-rule

---
+++ ChessVariantsLogic/Rules/MustCaptureRule.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChessVariantsLogic.Rules;
+
+public class MustCaptureRule : Rule
+{
+    public ISet<string> applyRule(Chessboard currentBoard, Chessboard boardCopy, string player)
+    {
+        
+    }
+}
+
---
+++ ChessVariantsLogic/Rules/Rule.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace ChessVariantsLogic.Rules;
+
+public interface Rule
+{
+    ISet<string> applyRule(Chessboard currentBoard, Chessboard boardCopy, string player);
+}
+
