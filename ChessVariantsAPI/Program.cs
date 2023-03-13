84fdb1e9d9b11ac1575f29680ce97ac34133f3e5
Johan Selin <johanselin99@gmail.com>
2023-03-13 22:51:17 +0100

    Added user controller utilizing data access project

--- ChessVariantsAPI/Program.cs
+++ ChessVariantsAPI/Program.cs
@@ -1,4 +1,7 @@
 using ChessVariantsAPI;
+using DataAccess.MongoDB;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +13,18 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSignalR();
 builder.Services.AddGameOrganzation();
+builder.Services.AddSingleton<DatabaseService>(new TestDatabaseService(builder.Configuration["MongoDatabase:ConnectionString"]));
+
+//builder.Services.AddAuthentication(options =>
+//{
+//    options.DefaultAuthenticateScheme = GoogleDefaults.AuthenticationScheme;
+//    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+//})
+//.AddGoogle(googleOptions =>
+//{
+//    googleOptions.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+//    googleOptions.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+//});
 
 var app = builder.Build();
 
@@ -25,9 +40,12 @@ if (app.Environment.IsDevelopment())
             .AllowCredentials());
 }
 
+
+
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseAuthorization();
+//app.UseAuthentication();
 
 app.MapControllers();
 app.MapHubs();
