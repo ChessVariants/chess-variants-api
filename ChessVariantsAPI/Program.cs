bdd531135e224358449f775aedbfafcf737b373b
Johan Selin <johanselin99@gmail.com>
2023-03-13 22:52:05 +0100

    Removed experimental google auth code

--- ChessVariantsAPI/Program.cs
+++ ChessVariantsAPI/Program.cs
@@ -1,7 +1,5 @@
 using ChessVariantsAPI;
 using DataAccess.MongoDB;
-using Microsoft.AspNetCore.Authentication.Google;
-using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,17 +12,6 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddSignalR();
 builder.Services.AddGameOrganzation();
 builder.Services.AddSingleton<DatabaseService>(new TestDatabaseService(builder.Configuration["MongoDatabase:ConnectionString"]));
-
-//builder.Services.AddAuthentication(options =>
-//{
-//    options.DefaultAuthenticateScheme = GoogleDefaults.AuthenticationScheme;
-//    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
-//})
-//.AddGoogle(googleOptions =>
-//{
-//    googleOptions.ClientId = builder.Configuration["Authentication:Google:ClientId"];
-//    googleOptions.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
-//});
 
 var app = builder.Build();
 
@@ -43,9 +30,6 @@ if (app.Environment.IsDevelopment())
 
 
 app.UseHttpsRedirection();
-
-//app.UseAuthorization();
-//app.UseAuthentication();
 
 app.MapControllers();
 app.MapHubs();
