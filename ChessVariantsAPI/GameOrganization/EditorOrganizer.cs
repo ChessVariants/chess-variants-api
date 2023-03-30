2a1a05be1202a5df2266f76a90a277c15bffcd35
ValleSten <valdemar.stenhammar@hotmail.com>
2023-03-30 16:08:27 +0200

    Add communication methods between API and Logic

--- ChessVariantsAPI/GameOrganization/EditorOrganizer.cs
+++ ChessVariantsAPI/GameOrganization/EditorOrganizer.cs
@@ -2,6 +2,9 @@ using ChessVariantsLogic.Editor;
 
 namespace ChessVariantsAPI;
 
+/// <summary>
+/// This class is responsible for communicating changes to the editor.
+/// </summary>
 public class EditorOrganizer
 {
     private readonly PieceEditor _pieceEditor;
@@ -11,15 +14,45 @@ public class EditorOrganizer
         this._pieceEditor = new PieceEditor();
     }
 
+    public string GetValidMoves(string square) { return _pieceEditor.GetAllCurrentlyValidMovesFromSquareAsJson(square); }
+
+    public string GetValidCaptures(string square) { return _pieceEditor.GetAllCurrentlyValidCapturesFromSquareAsJson(square); }
+
     public void AddMovementPattern(int xDir, int yDir, int minLength, int maxLength)
     {
         this._pieceEditor.AddMovementPattern(xDir, yDir, minLength, maxLength);
     }
 
-    public string GetStateAsJson()
+    public void AddCapturePattern(int xDir, int yDir, int minLength, int maxLength)
     {
-        return this._pieceEditor.ExportPieceAsJson();
+        this._pieceEditor.AddCapturePattern(xDir, yDir, minLength, maxLength);
     }
+
+    public void RemoveMovementPattern(int xDir, int yDir, int minLength, int maxLength)
+    {
+        this._pieceEditor.RemoveMovementPattern(xDir, yDir, minLength, maxLength);
+    }
+
+    public void RemoveCapturePattern(int xDir, int yDir, int minLength, int maxLength)
+    {
+        this._pieceEditor.RemoveCapturePattern(xDir, yDir, minLength, maxLength);
+    }
+
+    public void BelongsToPlayer(string player) { _pieceEditor.BelongsToPlayer(player); }
+
+    public void SameMovementAndCapture(bool enable) { _pieceEditor.SetSameMovementAndCapturePattern(enable); }
+
+    public void CanBeCaptured(bool enable) { _pieceEditor.SetCanBeCaptured(enable); }
+
+    public void RepeatMovement(int repeat) { _pieceEditor.RepeatMovement(repeat); }
+
+    public void SetRoyal(bool enable) { _pieceEditor.SetRoyal(enable); }
+
+    public void Build() { _pieceEditor.BuildPiece(); }
+
+    public void Reset() { _pieceEditor.ResetPiece(); }
+
+    public string GetStateAsJson() { return this._pieceEditor.ExportPieceAsJson(); }
 
 
 }
