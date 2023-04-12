2dfb791cd24dd79f572755cc877f1154fd740f14
ValleSten <valdemar.stenhammar@hotmail.com>
2023-04-12 23:43:06 +0200

    Refactor EditorHub

--- ChessVariantsAPI/Hubs/EditorHub.cs
+++ ChessVariantsAPI/Hubs/EditorHub.cs
@@ -19,92 +19,96 @@ public class EditorHub : Hub
         return _organizer.GetCurrentState();
     }
 
+    public PatternState RequestPatternState()
+    {
+        Console.WriteLine("In RequestPatternState editorHub");
+        return _organizer.GetCurrentPatternState();
+    }
+
+    public async Task UpdatePatternState()
+    {
+        var patternState = _organizer.GetCurrentPatternState();
+        await Clients.Caller.SendUpdatedPatternState(patternState);
+    }
+
+    public async Task UpdateEditorState()
+    {
+        var state = _organizer.GetCurrentState();
+        await Clients.Caller.SendUpdatedEditorState(state);
+    }
+
     public async Task ActivateSquare(string square)
     {
         Console.WriteLine("Activating square");
         _organizer.SetActiveSquare(square);
-        var state = _organizer.GetCurrentState();
-        await Clients.Caller.SendUpdatedEditorState(state);
+        await UpdateEditorState();
     }
 
     public async Task AddMovementPattern(int xDir, int yDir, int minLength, int maxLength)
     {
         Console.WriteLine("Adding movementPattern");
         _organizer.AddMovementPattern(xDir, yDir, minLength, maxLength);
-        var patternState = _organizer.GetCurrentPatternState();
-        var state = _organizer.GetCurrentState();
-        await Clients.Caller.SendUpdatedPatternState(patternState);
-        await Clients.Caller.SendUpdatedEditorState(state);
+        await UpdatePatternState();
+        await UpdateEditorState();
     }
 
     public async Task RemoveMovementPattern(int xDir, int yDir, int minLength, int maxLength)
     {
         Console.WriteLine("Removing movementPattern");
         _organizer.RemoveMovementPattern(xDir, yDir, minLength, maxLength);
-        var patternState = _organizer.GetCurrentPatternState();
-        var state = _organizer.GetCurrentState();
-        await Clients.Caller.SendUpdatedPatternState(patternState);
-        await Clients.Caller.SendUpdatedEditorState(state);
+        await UpdatePatternState();
+        await UpdateEditorState();
     }
 
     public async Task AddCapturePattern(int xDir, int yDir, int minLength, int maxLength)
     {
         Console.WriteLine("Adding CapturePattern");
         _organizer.AddCapturePattern(xDir, yDir, minLength, maxLength);
-        var state = _organizer.GetCurrentState();
-        var patternState = _organizer.GetCurrentPatternState();
-        await Clients.Caller.SendUpdatedPatternState(patternState);
-        await Clients.Caller.SendUpdatedEditorState(state);
+        await UpdatePatternState();
+        await UpdateEditorState();
     }
 
     public async Task UpdateBoardSize(int rows, int cols)
     {
         Console.WriteLine("Setting board size");
         _organizer.SetBoardSize(rows, cols);
-        var state = _organizer.GetCurrentState();
-        await Clients.Caller.SendUpdatedEditorState(state);
+        await UpdateEditorState();
     }
 
     public async Task SetCaptureSameAsMovement(bool enable)
     {
         Console.WriteLine("Setting capture and movement");
         _organizer.SameMovementAndCapture(enable);
-        var state = _organizer.GetCurrentState();
-        await Clients.Caller.SendUpdatedEditorState(state);
+        await UpdateEditorState();
     }
     
     public async Task ShowMovement(bool enable)
     {
         Console.WriteLine("Show movement or capture");
         _organizer.ShowMovement(enable);
-        var state = _organizer.GetCurrentState();
-        var patternState = _organizer.GetCurrentPatternState();
-        await Clients.Caller.SendUpdatedPatternState(patternState);
-        await Clients.Caller.SendUpdatedEditorState(state);
+        await UpdatePatternState();
+        await UpdateEditorState();
     }
 
     public async Task PieceCanBeCaptured(bool enable)
     {
         Console.WriteLine("Set CanBeCaptured");
         _organizer.CanBeCaptured(enable);
-        var state = _organizer.GetCurrentState();
-        await Clients.Caller.SendUpdatedEditorState(state);
+        await UpdateEditorState();
     }
 
     public async Task BelongsToPlayer(string player)
     {
         Console.WriteLine("Set belongs to player");
         _organizer.BelongsToPlayer(player);
-        var state = _organizer.GetCurrentState();
-        await Clients.Caller.SendUpdatedEditorState(state);
+        await UpdateEditorState();
     }
 
     public async Task AmountRepeat(int repeat)
     {
         Console.WriteLine("Set repeat");
         _organizer.RepeatMovement(repeat);
-        var state = _organizer.GetCurrentState();
-        await Clients.Caller.SendUpdatedEditorState(state);
+        await UpdateEditorState();
     }
 
 
