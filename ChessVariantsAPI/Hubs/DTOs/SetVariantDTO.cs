720f4ad90bd19c7b4a95a519234ee0cb9947cccd
bobby <gp.bobbelty@gmail.com>
2023-04-14 11:17:07 +0200

    CreateGame returns SetVariantDTO

---
+++ ChessVariantsAPI/Hubs/DTOs/SetVariantDTO.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace ChessVariantsAPI.Hubs.DTOs;
+
+public record SetVariantDTO
+{
+    [JsonProperty("success")]
+    public bool? Success { get; set; } = false;
+
+    [JsonProperty("failReason")]
+    public string? FailReason { get; set; }
+
+    public string AsJson()
+    {
+        return JsonConvert.SerializeObject(this, Formatting.Indented);
+    }
+}
