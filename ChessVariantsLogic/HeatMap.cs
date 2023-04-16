658462b84dc73a696aca3dd2a6b65199ddde81e4
AxelSiwmark1 <89916825+AxelSiwmark1@users.noreply.github.com>
2023-04-16 05:01:25 -0700

    heatmap

--- ChessVariantsLogic/HeatMap.cs
+++ ChessVariantsLogic/HeatMap.cs
@@ -5,35 +5,35 @@ public class HeatMap
     private int rows;
     private int cols;
 
-    private int[,] heatMap;
+    private double[,] heatMap;
 
     public HeatMap(int Rows, int Cols)
     {
         rows = Rows;
         cols = Cols;
-        heatMap = new int[Rows,Cols];
-        initHeatMap(4);
+        heatMap = new double[Rows,Cols];
+        initHeatMap(2);
         printMatrix(heatMap);
     }
 
 
-    private void initHeatMap(int weight)
+    private void initHeatMap(double weight)
     {
         int rowabc = rows/4;
         int colabc = cols/4;
         int min = Math.Min(rows,cols);
-      
-        int test = 1;
-        int index = 2;
-        while (index != 4)
+     
+        double value = weight /4;
+        int index = 1;
+        while (index <=4)
         {
             for (int row = index; row < rows; row++)
             {
                 for (int col = index; col < cols; col++)
                 {
-                    if ((col >= index && col <= cols - (index + 1) &&(row >= index && row <= rows - (index + 1))))
+                    if (col >= index && col <= cols - (index + 2) &&(row >= index && row <= rows - (index + 2)))
                     {
-                        heatMap[row, col]++;
+                        heatMap[row, col] += value;
                     }
                     
                 }
@@ -43,16 +43,16 @@ public class HeatMap
         
     }
 
-    public int GetValue(int row, int col)
+    public double GetValue(int row, int col)
     {
         return heatMap[row, col];
     }
 
    
-        static void printMatrix(int[,] m)
+        static void printMatrix(double[,] m)
         {
             for(int i = 0; i < m.GetLength(0); i++){
-                for(int j = 0; j < m.GetLength(0); j++){
+                for(int j = 0; j < m.GetLength(1); j++){
                     Console.Write("{0} ", m[i,j]);
                 }
                 Console.WriteLine();
