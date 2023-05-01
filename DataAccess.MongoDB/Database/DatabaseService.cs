b2237d1c8759f32a6d70dc9621c04fe873b91707
Joakim Holmberg <joakimho@student.chalmers.se>
2023-05-01 19:59:52 +0200

    Initial work on database integration for ruleset editor

--- DataAccess.MongoDB/Database/DatabaseService.cs
+++ DataAccess.MongoDB/Database/DatabaseService.cs
@@ -1,4 +1,5 @@
-﻿using MongoDB.Driver;
+﻿using DataAccess.MongoDB.Repositories;
+using MongoDB.Driver;
 
 namespace DataAccess.MongoDB;
 
@@ -11,11 +12,13 @@ public abstract class DatabaseService
     private readonly IMongoClient _client;
     private readonly IMongoDatabase _database;
     public readonly UserRepository Users;
+    public readonly PredicateRepository Predicates;
 
     public DatabaseService(string connectionString, string databaseName)
     {
         _client = new MongoClient(connectionString);
         _database = _client.GetDatabase(databaseName);
         Users = new UserRepository(_database);
+        Predicates = new PredicateRepository(_database);
     }
 }
