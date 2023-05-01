ad6cee9d8d3948f0604fe6858ad6e3fe570abce3
Joakim Holmberg <joakimho@student.chalmers.se>
2023-05-01 20:36:41 +0200

    more work

--- ChessVariantsAPI/Controllers/Rules/PredicateController.cs
+++ ChessVariantsAPI/Controllers/Rules/PredicateController.cs
@@ -1,10 +1,13 @@
-﻿using DataAccess.MongoDB;
+﻿using ChessVariantsAPI.DTOs;
+using DataAccess.MongoDB;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
+using DataAccess.MongoDB.Models;
 
 namespace ChessVariantsAPI.Controllers;
 
+[Authorize]
 [Route("api/[controller]")]
 [ApiController]
 public class PredicateController : GenericController
@@ -17,10 +20,38 @@ public class PredicateController : GenericController
     }
 
     [HttpGet]
-    public async Task GetByUserName()
+    public async Task<IActionResult> GetByUserName()
     {
         _logger.LogDebug("logging some");
-        var username = User?.FindFirst(ClaimTypes.Name)?.Value!;
+        var username = GetUsername();
+        if (username == null || username == "")
+        {
+            _logger.LogDebug("User tried to request predicates but was either not logged in or not authorized for the action.");
+            return Unauthorized("User is either not logged in or not authorized for this action.");
+        }
+
         _logger.LogInformation("User {u} requested predicates", username);
+        return Ok();
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> SavePredicate(SavePredicateDTO dto)
+    {
+        var pred = new Predicate
+        {
+            CreatorName = GetUsername(),
+            Name = dto.Name,
+            Code = dto.Code,
+            Description = dto.Description,
+        };
+        _logger.LogDebug("Trying to save pred: \n{p}", pred);
+        await _db.Predicates.CreateAsync(pred);
+
+        return Ok();
+    }
+
+    private string GetUsername()
+    {
+        return User?.FindFirst(ClaimTypes.Name)?.Value!;
     }
 }
