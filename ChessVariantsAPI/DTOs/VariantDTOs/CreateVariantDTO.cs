7fb8f44a84a32879493ff7af5a26fce57f9f4712
Johan Selin <johanselin99@gmail.com>
2023-05-08 19:02:44 +0200

    work

---
+++ ChessVariantsAPI/DTOs/VariantDTOs/CreateVariantDTO.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChessVariantsAPI.DTOs.VariantDTOs;
+
+public record CreateVariantDTO
+{
+    [Required]
+    public string Name { get; set; } = null!;
+
+    [Required]
+    public string Description { get; set; } = null!;
+
+    [Required]
+    public string WhiteRuleSetIdentifier { get; set; } = null!;
+
+    [Required]
+    public string BlackRuleSetIdentifier { get; set; } = null!;
+
+    [Required]
+    public string BoardIdentifier { get; set; } = null!;
+
+    [Required]
+    public int MovesPerTurn { get; set; } = 1;
+}
---
+++ DataAccess.MongoDB/Repositories/VariantRepository.cs
@@ -0,0 +1,17 @@
+﻿using DataAccess.MongoDB.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.MongoDB.Repositories;
+public class VariantRepository : GenericRepository<Variant>
+{
+    public const string CollectionName = "Variants";
+
+    public VariantRepository(IMongoDatabase database) : base(database.GetCollection<Variant>(CollectionName))
+    {
+    }
+}
